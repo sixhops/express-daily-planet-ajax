804b3ef007a321866940ca56bed7bbcf7ea8dcbe
shullmb <shullmb@gmail.com>
2018-06-14 19:28:17 -0700

    fixed new route, added post route

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
+var db = require('./models');
+var layouts = require('express-ejs-layouts');
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/static'));
+app.use(layouts);
 
-var articles = [
-  { title: 'Bernie! Bernie!', body: '#feelthebern' },
-  { title: 'Trump for change!', body: 'Make America Great Again' },
-  { title: 'Brian Hague founds the Daily Planet', body: 'Wow! Amazing! Such good news!' }
-];
+// var articles = [
+//   { title: 'Bernie! Bernie!', body: '#feelthebern' },
+//   { title: 'Trump for change!', body: 'Make America Great Again' },
+//   { title: 'Brian Hague founds the Daily Planet', body: 'Wow! Amazing! Such good news!' }
+// ];
 
 // GET / - gets the main site index page
 app.get('/', function(req, res) {
@@ -23,30 +26,40 @@ app.get('/about', function(req, res) {
 
 // GET /articles - gets full articles list
 app.get('/articles', function(req, res) {
-  // TODO: Add db access code here.
-  res.render('articles/index', { articles: articles });
-});
-
-// GET /articles/:index - gets a specific article
-app.get('/articles/:index', function(req, res) {
-  var index = parseInt(req.params.index);
-  if (index < articles.length && index >= 0) {
-    // TODO: Add db access code here.
-    res.render('articles/show', { article: articles[req.params.index] });
-  } else {
-    res.send('Error');
-  }
+  db.article.findAll().then(function(articles){
+    res.render('articles/index', { articles: articles });
+  });
 });
 
 // GET /articles/new - returns form for new article
 app.get('/articles/new', function(req, res) {
   res.render('articles/new');
 });
+
+// GET /articles/:index - gets a specific article
+app.get('/articles/:index', function(req, res) {
+  // TODO: update error checking because there is no articles object
+    db.article.find({
+      where: {index: req.params.index}
+    }).then( function(article) {
+      res.render('articles/show', {article: article});
+    })
+});
+
+
 // POST /articles - create a new article from form data
 app.post('/articles', function(req, res) {
   // TODO: Add db access code here.
-  res.redirect('/articles');
+  db.article.create({
+    title: req.body.title,
+    body: req.body.body
+  }).then( function(article) {
+    res.redirect('/articles');
+  })
 });
+
+//TODO  ADD PUT & DELETE ROUTES
+
 
 app.listen(3000, function() {
     console.log("You're listening to the smooth sounds of port 3000 in the morning");
