f21b76b5cc623726101881a9d544a11706a034ed
Genevieve Schultz <genschultz@gmail.com>
2018-06-14 20:19:05 -0700

    all modules installed

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var db = require('./models');
 var app = express();
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/static'));
 
-var articles = [
-  { title: 'Bernie! Bernie!', body: '#feelthebern' },
-  { title: 'Trump for change!', body: 'Make America Great Again' },
-  { title: 'Brian Hague founds the Daily Planet', body: 'Wow! Amazing! Such good news!' }
-];
+// var articles = [
+//   { title: 'Bernie! Bernie!', body: '#feelthebern' },
+//   { title: 'Trump for change!', body: 'Make America Great Again' },
+//   { title: 'Brian Hague founds the Daily Planet', body: 'Wow! Amazing! Such good news!' }
+// ];
 
 // GET / - gets the main site index page
 app.get('/', function(req, res) {
@@ -21,15 +22,16 @@ app.get('/about', function(req, res) {
   res.render('about');
 });
 
-// GET /articles - gets full articles list
+// GET /articles - gets full articles list (find all)
 app.get('/articles', function(req, res) {
   // TODO: Add db access code here.
   res.render('articles/index', { articles: articles });
 });
 
-// GET /articles/:index - gets a specific article
+// GET /articles/:index - gets a specific article (get one)
 app.get('/articles/:index', function(req, res) {
   var index = parseInt(req.params.index);
+  // TODO: Update this error checking to look at the database (or just remove it)
   if (index < articles.length && index >= 0) {
     // TODO: Add db access code here.
     res.render('articles/show', { article: articles[req.params.index] });
@@ -47,6 +49,10 @@ app.post('/articles', function(req, res) {
   // TODO: Add db access code here.
   res.redirect('/articles');
 });
+
+// TODO: PUT
+
+// TODO: DELETE
 
 app.listen(3000, function() {
     console.log("You're listening to the smooth sounds of port 3000 in the morning");
