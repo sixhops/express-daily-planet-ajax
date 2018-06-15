04c8a4a92c28ff529a883c0818be24151f91c45d
Matt Carver <mattcarver.wd@gmail.com>
2018-06-15 09:01:39 -0700

    setting up sequelize

---
+++ static/js/script.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+  $('form').on('submit', function(e) {
+    e.preventDefault();
+    var newData = $(this).serialize();
+    var url = $(this).attr('action');
+    $.ajax({
+      method: "PUT",
+      url: url,
+      data: newData
+    }).done(function(data) {
+      console.log(data);
+      window.location = '/articles';
+    });
+  });
+
+  $('a').on('click', function(e) {
+    e.preventDefault();
+    var url = $(this).attr('href');
+    $.ajax({
+      method: "DELETE",
+      url: url
+    }).done(function(data) {
+      console.log(data);
+      window.location = '/articles';
+    });
+  });
+});
