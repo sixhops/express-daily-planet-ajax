32f9d54cdd75695c3f6a34041e5115d6cdf06456
shullmb <shullmb@gmail.com>
2018-06-15 10:34:18 -0700

    added dotenv

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require('express');
+require('dotenv').config();
 var bodyParser = require('body-parser');
 var app = express();
 var db = require('./models');
 var layouts = require('express-ejs-layouts');
+var port = process.env.PORT || 2000;
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -95,5 +97,5 @@ app.get('*', function(req,res) {
 })
 
 app.listen(3000, function() {
-    console.log("You're listening to the smooth sounds of port 3000 in the morning");
+    console.log("You're listening to the smooth sounds of port " + port + " in the morning");
 });
--- models/article.js
+++ models/article.js
@@ -2,7 +2,7 @@
 module.exports = (sequelize, DataTypes) => {
   var article = sequelize.define('article', {
     title: DataTypes.STRING,
-    body: DataTypes.STRING
+    body: DataTypes.STRING,
   }, {});
   article.associate = function(models) {
     // associations can be defined here
