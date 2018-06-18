2d7979324d7779f07136b5475854e4bd8d4c6a36
rkvasisht <rkvasisht@gmail.com>
2018-06-17 21:51:26 -0700

    added views

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,30 @@
-
+// $(document).ready(function(){
+//   $('.edit-form').on('submit', function(e){
+//     e.preventDefault();
+//     console.log('clicked!')
+//     var updatedInfo =$(this).serialize();
+//     var url= $(this).attr('action');
+//
+//
+//     $.ajax({
+//       method: 'PUT',
+//       url: url,
+//       data: updatedInfo
+//     }).done(function(data){
+//       console.log(data);
+//       window.location ='/articles'
+//     })
+//
+//
+//   })
+//   $('a.delete').click(function(e){
+//     e.preventDefault();
+//     var url = $(this).attr('href');
+//     $.ajax({
+//       method: 'DELETE',
+//       url: url
+//     }).done(function(data){
+//       window.location = '/articles';
+//     })
+//   })
+// })
